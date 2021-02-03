94a63f4663853ef0d9b94f251619242345b1d909
Ririko Nagaishi <riry.cucumber@gmail.com>
2021-02-04 00:21:05 +0900

    ガチャ

---
+++ Gacha/ResultViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ResultViewController.swift
+//  Gacha
+//
+//  Created by Ririko Nagaishi on 2021/02/04.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    //乱数を入れるためのInt型の変数を用意
+    var number: Int!
+    
+    //背景画面とモンスターの画像を表示するためのImageViewを用意
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var monsterImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    //0-9までのランダムな数を発生させる
+        number = Int.random(in: 0...9)
+        if number == 9 {
+            monsterImageView.image = UIImage(named: "monster010")
+            backgroundImageView.image = UIImage(named: "bg_gold")
+        }else if number > 7 {
+            monsterImageView.image = UIImage(named: "monster006")
+            backgroundImageView.image = UIImage(named: "bg_red")
+        }else {
+            monsterImageView.image = UIImage(named: "monster003")
+            backgroundImageView.image = UIImage(named: "bg_blue")
+        }
+    }
+    
+        @IBAction func back() {
+            self.dismiss(animated: true, completion: nil)
+        }
+    
+    
+    
+    }
+    
+
+    
+    
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
